3d8ba56d0492622f9f0f9e2b5cf9aa0e8949cf67
Bill Forney <william@williamforney.com>
2025-07-05 19:49:29 -0700

    Add SunEphemeris class for solar calculations
    Introduces a new static class `SunEphemeris` in the `Ephemeris.Heliology` namespace. This class features two methods: `HeliocentricLongitude`, which calculates the heliocentric longitude and radius vector using VSOP87 coefficients, and `ApparentEquatorialCoordinates`, which computes Right Ascension and Declination based on the heliocentric longitude and mean obliquity of the ecliptic. Additional using directives are included for necessary namespaces.

---
+++ Ephemeris/Heliology/SunEphemeris.cs
@@ -0,0 +1,36 @@
+﻿using Ephemeris.Chronology;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ephemeris.Heliology;
+
+public static class SunEphemeris
+{
+    public static (double longitude, double radiusVector) HeliocentricLongitude(double T)
+    {
+        // Simplified high-accuracy VSOP87 coefficients for L0
+        double L0 = 280.46646 + 36000.76983 * T + 0.0003032 * T * T;
+        return (TimeUtils.NormalizeDegrees(L0), 1.00014 - 0.01671 * Math.Cos(TimeUtils.ToRadians(357.529 + 35999.050 * T)));
+    }
+
+    public static (double RA, double Dec) ApparentEquatorialCoordinates(double T)
+    {
+        var (L, _) = HeliocentricLongitude(T);
+
+        // Mean obliquity of the ecliptic (deg)
+        double epsilon0 = 23.43929111 - 0.0130042 * T;
+
+        // Convert to radians
+        double Lrad = TimeUtils.ToRadians(L);
+        double epsRad = TimeUtils.ToRadians(epsilon0);
+
+        // RA/Dec in degrees
+        double RA = TimeUtils.NormalizeDegrees(TimeUtils.ToDegrees(Math.Atan2(Math.Cos(epsRad) * Math.Sin(Lrad), Math.Cos(Lrad))));
+        double Dec = TimeUtils.ToDegrees(Math.Asin(Math.Sin(epsRad) * Math.Sin(Lrad)));
+
+        return (RA, Dec);
+    }
+}
